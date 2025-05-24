fbb31620416fdaa7ab65897b00e827492c0d767f
KamilG <gierczyckik@gmail.com>
2025-05-24 22:13:56 +0200

    dodanie opcji pwa

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,35 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+    sveltekit(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'pwa-512x512.png'],
+      manifest: {
+        name: 'Kto mnie zastawia',
+        short_name: 'Parking',
+        description: 'Aplikacja do oznaczania zajętych miejsc parkingowych',
+        theme_color: '#ffffff',
+        background_color: '#f0f0f0',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      srcDir: 'src',
+      filename: 'service-worker.js',
+      devOptions: {
+        enabled: true,
+        type: 'module'
+      }
+    })
+  ]
 });
